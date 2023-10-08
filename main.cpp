d1963f7cdd7541f11be9a677e75f35726b970f33
daniel <danieloliveira.s558@gmail.com>
2023-10-08 10:59:06 -0300

    Versão integrada

--- main.cpp
+++ main.cpp
@@ -7,153 +7,283 @@ using namespace std;
 
 #define TRACO "------------------------------------------------"
 
-class Produto {
-    private:
-        static int codigo_atual;
-        int codigo;
-        string nome;
-        double preco;
+class Produto
+{
+private:
+    int codigo;
+    string nome;
+    double preco;
 
-    public:
-        ~Produto();
-        Produto(string _nome, double _preco);
-        int getCodigo();
-        string getNome();
-        double getPreco();
-        void setPreco(double _preco);
+public:
+    ~Produto();
+    Produto(string _nome, double _preco);
+    Produto(int _codigo, string _nome, double _preco);
+    int getCodigo();
+    string getNome();
+    double getPreco();
+    void setPreco(double _preco);
+    void setCodigo(int _codigo)
+    {
+        codigo = _codigo;
+    }
 };
 
-// Inicializa o código_atual do produto
-int Produto::codigo_atual = 0;
-
-Produto :: ~Produto() {
-    
+Produto ::~Produto()
+{
 }
+// Produto:: Produto(int _codigo, string _nome, double _preco){
+//         codigo = _codigo;
+//         nome = _nome;
+//         preco = _preco;
+// }
 
-Produto :: Produto(string _nome, double _preco){
-    codigo_atual++;
-    codigo = codigo_atual;
+Produto ::Produto(string _nome, double _preco)
+{
+
     nome = _nome;
     preco = _preco;
 };
 
-int Produto :: getCodigo(){
+int Produto ::getCodigo()
+{
     return codigo;
 };
 
-string Produto :: getNome(){
+string Produto ::getNome()
+{
     return nome;
 };
 
-double Produto :: getPreco(){
+double Produto ::getPreco()
+{
     return preco;
 };
 
-void Produto :: setPreco(double _preco){
+void Produto ::setPreco(double _preco)
+{
     preco = _preco;
 };
 
+
+class Estoque
+{
+private:
+    static int codigo_atual;
+    vector<Produto> itens_em_estoque;
+    vector<int> qtd_em_estoque;
+
+public:
+    Estoque(){};
+    int buscaItem_em_estoque(string nome_a_buscar)
+    {
+
+        for (int i = 0; i < itens_em_estoque.size(); ++i)
+        {
+            if (itens_em_estoque[i].getNome() == nome_a_buscar)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void cadastrarProduto(Produto &novoItem, int qtd)
+    {
+        if (buscaItem_em_estoque(novoItem.getNome())  != -1)
+        {
+            adicionarProduto(novoItem, qtd);
+            return;
+        }
+        codigo_atual++;
+        novoItem.setCodigo(codigo_atual);
+
+        itens_em_estoque.push_back(novoItem);
+        qtd_em_estoque.push_back(qtd);
+    }
+
+    void adicionarProduto(Produto &novoItem, int qtd)
+    {
+        // verificação e o item já está cadastrado e somar a quatidade;
+        int posicao_do_item = buscaItem_em_estoque(novoItem.getNome());
+
+        // se cadastrado adicione a quantidade;
+        if (posicao_do_item != -1)
+        {
+            qtd_em_estoque[posicao_do_item] += qtd;
+        }
+        else
+        {
+            // se não tiver cadastrado cadastre;
+            cadastrarProduto(novoItem, qtd);
+        }
+    }
+
+    bool moverProduto(Produto Item, int qtd)
+    {
+        int posicao_do_item = buscaItem_em_estoque(Item.getNome());
+
+        if (posicao_do_item != -1)
+        {
+            int quantidade = qtd_em_estoque[posicao_do_item];
+
+            if ((quantidade - qtd) >= 0)
+            {
+                qtd_em_estoque[posicao_do_item] -= qtd;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    void listarEstoque()
+    {
+        cout << "id  "  <<"Nome  " << "valor  " << "Qtd"<<endl;
+        for (int i = 0; i < itens_em_estoque.size(); ++i)
+        {
+            cout << "#" << itens_em_estoque[i].getCodigo() << "  " << itens_em_estoque[i].getNome() << "  " << itens_em_estoque[i].getPreco() <<" x " << qtd_em_estoque[i] << std::endl;
+        }
+    }
+};
+int Estoque::codigo_atual = 0;
+
+
+
 class CarrinhoDeCompras
 {
-    private:
-        vector<Produto> carrinho;
-        vector<int> quantidade;
-    public:
-        ~CarrinhoDeCompras();
-        CarrinhoDeCompras();
-        void adicionaProduto(Produto& _produto, int _quantidade);
-        void removeProduto(int codigo, int _quantidade);
-        double calculaTotal(vector<Produto> carrinho);
-        void exibeCarrinho(vector<Produto> carrinho);
+private:
+    vector<Produto> carrinho;
+    vector<int> quantidade;
 
-    
+public:
+    ~CarrinhoDeCompras();
+    CarrinhoDeCompras();
+    bool adicionarProduto(Produto &_produto, int _quantidade);
+    bool removerProduto(Produto _produto ,int _quantidade);
+    double calcularTotal();
+    void exibirCarrinho();
 };
 
-CarrinhoDeCompras::CarrinhoDeCompras() 
-{
-}
+CarrinhoDeCompras::CarrinhoDeCompras() {}
 
 CarrinhoDeCompras::~CarrinhoDeCompras()
 {
 }
 
-void CarrinhoDeCompras::adicionaProduto(Produto& _produto, int _quantidade)
+bool CarrinhoDeCompras::adicionarProduto(Produto &_produto, int _quantidade)
 {
-    carrinho.push_back(_produto);
-    quantidade.push_back(_quantidade);
-
+    Estoque retirarItem;
+    if(retirarItem.moverProduto(_produto, _quantidade)){
+        carrinho.push_back(_produto);
+        quantidade.push_back(_quantidade);
+        return true;
+    }else{
+        return false;
+    }
+    
 }
 
-void CarrinhoDeCompras::removeProduto(int _codigo, int _quantidade)
+bool CarrinhoDeCompras::removerProduto(Produto _produto ,int _quantidade)
 {
     int tamanho = carrinho.size();
 
-    for (int i = 0; i < tamanho; i++) {
-        if (carrinho[i].getCodigo() == _codigo) {
-            if (quantidade[i] >= _quantidade) {
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (carrinho[i].getNome() == _produto.getNome())
+        {
+            if (quantidade[i] >= _quantidade)
+            {
                 quantidade[i] -= _quantidade;
-                if (quantidade[i] == 0) {
+                if (quantidade[i] == 0)
+                {
                     carrinho.erase(carrinho.begin() + i);
                     quantidade.erase(quantidade.begin() + i);
+                    return true;
                 }
-                return;
-            } else {
-                cout << "Quantidade insuficiente no carrinho." << endl;
-                return;
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
     cout << "Produto não encontrado no carrinho." << endl;
 }
 
-double CarrinhoDeCompras::calculaTotal(vector<Produto> carrinho) {
+double CarrinhoDeCompras::calcularTotal()
+{
     double total = 0;
-
-    for (Produto produto : carrinho)
+    // Certifique-se de que ambas as listas tenham o mesmo tamanho
+    if (carrinho.size() != quantidade.size())
     {
-        total += produto.getPreco();
+        // Trate o erro aqui, por exemplo, lançando uma exceção
+        throw std::runtime_error("Tamanho das listas carrinho e quantidade não corresponde.");
+    }
+
+    // Agora, percorra os elementos do carrinho e calcule o total
+    for (int i = 0; i < carrinho.size(); i++)
+    {
+        total += carrinho[i].getPreco() * quantidade[i];
     }
 
     return total;
 }
 
-void CarrinhoDeCompras::exibeCarrinho(vector<Produto> carrinho) {
-    cout << TRACO << endl;
-    cout << left << setw(16) << "Código" << setw(16) << "Nome" << setw(16) << "Preço" << endl;
-    cout << TRACO << endl;
-    for (Produto produto : carrinho)
+void CarrinhoDeCompras::exibirCarrinho()
+{
+
+    cout << endl
+         << "Resposta: Carinho de Compras:" << endl;
+    for (int i = 0; i < carrinho.size(); i++)
     {
-        cout << left << setw(16) << produto.getCodigo() << setw(16) << produto.getNome() << setw(16) << produto.getPreco() << endl;
+        cout << "-" << carrinho[i].getNome() << " (" << carrinho[i].getPreco() << ") "
+             << " x " << quantidade[i] << endl;
     }
+    cout << endl;
 }
 
-int main(){
-    //Testes
-    CarrinhoDeCompras carrinho;
-    vector<Produto> produtos;
 
+int main()
+{
+
+    // git vector<Estoque> produtos;//usar esse para registrar no estoque;
+    // Testes para cadastro em estoque
+    //          id  nome  preco
     Produto p1("Maçã", 2.5);
     Produto p2("Arroz", 10.0);
     Produto p3("Leite", 4.0);
     Produto p4("Café", 8.99);
 
-    produtos.push_back(p1);
-    produtos.push_back(p2);
-    produtos.push_back(p3);
-    produtos.push_back(p4);
+    Estoque estoque;
+    
+    estoque.cadastrarProduto(p1, 10);
+    estoque.cadastrarProduto(p2, 10);
+    estoque.cadastrarProduto(p3, 10);
+    estoque.cadastrarProduto(p4, 10);
 
-    cout << "Total: " <<  carrinho.calculaTotal(produtos) << endl;
+    cout <<"Itens disponiveis:" << endl;
+    estoque.listarEstoque();
 
-    carrinho.exibeCarrinho(produtos);
+    // Testes para carrinho de compras;
+  
 
-    cout << endl;
+    CarrinhoDeCompras carrinho;
+    carrinho.adicionarProduto(p1, 3);
+    carrinho.adicionarProduto(p2, 2);
+    carrinho.adicionarProduto(p3, 3);
+    
+    double valor = carrinho.calcularTotal();
 
+    carrinho.exibirCarrinho();
+    cout << "Total: " << valor << endl;
 
-    cout << fixed << setprecision(2);
-    cout << "Produto " << p1.getCodigo() << ":" << "\t" << p1.getNome() << "\t"  << p1.getPreco() << endl;
-    cout << "Produto " << p2.getCodigo() << ":" << "\t" << p2.getNome() << "\t"  << p2.getPreco() << endl;
-    cout << "Produto " << p3.getCodigo() << ":" << "\t" << p3.getNome() << "\t"  << p3.getPreco() << endl;
-    cout << "Produto " << p4.getCodigo() << ":" << "\t" << p4.getNome() << "\t"  << p4.getPreco() << endl;
+    carrinho.removerProduto(p3, 1);
+    carrinho.exibirCarrinho();
+    valor = carrinho.calcularTotal();
+    cout << "Total: " << valor << endl;
 
     return 0;
 }
